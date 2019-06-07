ade396e799e08b6f8fb40634c615a85ba09c6c04
Tang4109 <2298964704@qq.com>
2019-06-07 16:03:12 +0800

    leetcode 2 买股票的最佳时机

---
+++ 初级算法/初级算法/2_买股票的最佳时机.cpp
@@ -0,0 +1,74 @@
+/****************************************!
+*@brief  leetcode 2 买股票的最佳时机
+*@author ZhangYunjia
+*@date   2019/6/7/11:52
+
+****************************************/
+
+#include<iostream>
+#include <vector>
+using namespace std;
+
+#define GET_ARRAY_LEN(array,len){len = (sizeof(array) / sizeof(array[0]));}
+class Solution {
+public:
+	int maxProfit(vector<int>& a) {
+		len = a.size();
+		if (len == 0)
+		{
+			return 0;
+		}
+		//{ 1,1,2,2,3,3,3,4,1,0,2}
+		for (int i = 0; i < len-1; i++)
+		{
+			if (a[i] > a[i+1])
+			{
+				if (k!=0)
+				{
+					profit = profit + (a[i] - a[i - k]);
+				}
+				k = 0;
+			}
+			else if (a[i] <= a[i + 1])
+			{
+				k++;
+				if (i == len - 2)
+				{
+					profit = profit + a[len - 1] - a[len-1-k];
+				}
+
+			}
+
+		}
+		
+		return profit;
+	}
+
+private:
+	int len;
+	int profit = 0;
+	int k=0;
+};
+
+int main()
+{
+	int len = 0;
+	//int a[] = { 6, 1, 3, 2, 4, 7 };
+	//int a[] = { 7,6,4,3,1 };
+	//int a[] = { 1,2,3,4,5 };
+	int a[] = { 5,2,3,2,6,6,2,9,1,0,7,4,5,0 };
+	vector<int> v;
+	GET_ARRAY_LEN(a, len);
+	for (int i = 0; i < len; i++)
+	{
+		v.push_back(a[i]);
+	}
+	int profit = 0;
+	Solution s;
+	profit = s.maxProfit(v);
+	cout << profit << endl;
+
+	cout << "\nend.." << endl;
+	system("pause");
+	return 0;
+}
