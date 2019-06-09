34b5548de414c44475b727b1e5527ffd44c1ca0f
Tang4109 <2298964704@qq.com>
2019-06-09 12:01:31 +0800

    4_判断数组是否存在重复元素

---
+++ 初级算法/初级算法/4_存在重复.cpp
@@ -0,0 +1,60 @@
+/****************************************!
+*@brief  4_判断数组是否存在重复元素
+*@author ZhangYunjia
+*@date   2019/6/9/11:28
+
+****************************************/
+
+#include<iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+//方法1：时间复杂度太高
+class Solution1 {
+public:
+	bool containsDuplicate(vector<int>& a) {
+		len = a.size();
+		cout << len << endl;
+		for (int i=0;i<len;i++)
+		{
+			for (int j=i+1;j<len;j++)
+			{
+				if (a[j]==a[i])
+				{
+					return true;
+				}
+			}
+		}
+		
+
+		return false;
+	}
+private:
+	int len;
+};
+//方法2：利用set集合不能放重复元素的特点实现
+class Solution {
+public:
+	bool containsDuplicate(vector<int>& a) {
+		set<int> s;
+		for (auto x : a) {
+			s.insert(x);
+		}
+		return s.size() != a.size();
+	}
+};
+
+
+int main()
+{
+	vector<int> a = { 1,2,3,4 };
+	Solution s;
+	int flag;
+	flag = s.containsDuplicate(a);
+	cout << flag << endl;
+	
+	cout << "\nend.." << endl;
+	system("pause");
+	return 0;
+}
